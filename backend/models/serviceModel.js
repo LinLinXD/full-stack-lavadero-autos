466a318093c7186ffd3424492500668f810320e0
Liang Camilo <liangcamilo2006@gmail.com>
2025-10-06 16:29:04 -0500

    minor changes

--- backend/models/serviceModel.js
+++ backend/models/serviceModel.js
@@ -1,6 +1,6 @@
 import { model } from "mongoose";
 import servicioSchema from "../schemas/servicioSchema.js";
 
-const serviceModel = model("Reservas", servicioSchema)
+const serviceModel = model("Servicios", servicioSchema)
 
 export default serviceModel
