fe4f2377c62d51be0f71e1d1e42079d39d663037
Liang Camilo <liangcamilo2006@gmail.com>
2025-10-11 19:00:25 -0500

    fix: small fixes to 'reservaSchema'

--- backend/schemas/reservaSchema.js
+++ backend/schemas/reservaSchema.js
@@ -1,27 +1,27 @@
 import {Schema} from "mongoose";
 
 const reservaSchema = Schema({
-    id_servicio: {
+    id_servicio: [{
         type: Schema.Types.ObjectId,
         ref: "Servicios",
         required: true
-    },
+    }],
     id_usuario: {
         type: Schema.Types.ObjectId,
         ref: "Usuarios"
     },
-    id_vehiculo: {type: String, required: true},
+    placa: {
+        type: String, 
+        required: true
+    },
     estado: {
         type: String,
         enum: ["activo", "completado", "cancelado"]
     },
-    fecha_reservacion: {
+    fecha: {
         type: Date,
-        default:Date.now
+        required: true
     },
-    fecha_finalizado: {
-        type: Date
-    } 
 })
 
 export default reservaSchema
