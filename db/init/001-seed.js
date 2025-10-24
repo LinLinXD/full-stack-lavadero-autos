87913636dc53789be0229a5b5203b81d6bf0e43f
Liang Camilo <liangcamilo2006@gmail.com>
2025-10-24 15:21:06 -0500

    modified seeder for url and categoria properties

--- db/init/001-seed.js
+++ db/init/001-seed.js
@@ -5,25 +5,25 @@ const db = db.getSiblingDB(dbName);
 // Crea colección 'servicios' con algunos documentos
 db.servicios.insertMany([
   // ---- Lavados ----
-  { nombre: "lavado-basico", descripcion: "Lavado exterior con agua y jabón, sin encerado.", costo: 15000, duracion: 20, excluye: ["lavado-premium", "lavado-completo"] },
-  { nombre: "lavado-premium", descripcion: "Lavado exterior + secado detallado + brillo en llantas.", costo: 30000, duracion: 45, excluye: ["lavado-basico", "lavado-completo"] },
-  { nombre: "lavado-completo", descripcion: "Lavado exterior e interior, incluye aspirado y limpieza básica de tapicería.", costo: 40000, duracion: 60, excluye: ["lavado-basico", "lavado-premium", "aspirado-interior"] },
+  { nombre: "Lavado Basico", categoria: "lavado", descripcion: "Lavado exterior con agua y jabón, sin encerado.", costo: 15000, duracion: 20, excluye: ["Lavado Premium", "Lavado Completo"], url: 'https://i.postimg.cc/Gp3hdPvv/lavado-basico.jpg' },
+  { nombre: "lavado premium", categoria: "lavado", descripcion: "Lavado exterior + secado detallado + brillo en llantas.", costo: 30000, duracion: 45, excluye: ["Lavado Basico", "Lavado Completo"], url: 'https://i.postimg.cc/L53b9Pt9/lavado-premium.jpg'},
+  { nombre: "Lavado Completo", categoria: "lavado", descripcion: "Lavado exterior e interior, incluye aspirado y limpieza básica de tapicería.", costo: 40000, duracion: 60, excluye: ["Lavado Basico", "Lavado Premium", "Aspirado interior"], url: 'https://i.postimg.cc/6pgYNckR/lavado-completo.jpg'},
 
   // ---- Interior ----
-  { nombre: "aspirado-interior", descripcion: "Aspirado de alfombras y asientos, limpieza ligera.", costo: 10000, duracion: 15, excluye: ["lavado-completo", "limpieza-interior-premium"] },
-  { nombre: "limpieza-tapiceria", descripcion: "Limpieza profunda de tapicería y alfombras.", costo: 25000, duracion: 45, excluye: ["limpieza-interior-premium"] },
-  { nombre: "limpieza-interior-premium", descripcion: "Limpieza completa de interior con tapicería, plásticos y aspirado profundo.", costo: 35000, duracion: 60, excluye: ["aspirado-interior", "limpieza-tapiceria"] },
+  { nombre: "Aspirado Interior", categoria: "interior", descripcion: "Aspirado de alfombras y asientos, limpieza ligera.", costo: 10000, duracion: 15, excluye: ["Lavado Completo", "Limpieza Interior Premium"], url: 'https://i.postimg.cc/KzqbZdm2/aspirado-interior.jpg' },
+  { nombre: "Limpieza Tapiceria", categoria: "interior", descripcion: "Limpieza profunda de tapicería y alfombras.", costo: 25000, duracion: 45, excluye: ["Limpieza Interior Premium"], url: 'https://i.postimg.cc/bvPSb2bV/limpieza-tapiceria.jpg'},
+  { nombre: "Limpieza Interior Premium", categoria: "interior", descripcion: "Limpieza completa de interior con tapicería, plásticos y aspirado profundo.", costo: 35000, duracion: 60, excluye: ["Aspirado Interior", "Limpieza Tapiceria"], url: 'https://i.postimg.cc/52dXdFPg/limpieza-interior-premium.jpg' },
 
   // ---- Tratamientos especiales ----
-  { nombre: "encerado", descripcion: "Aplicación de cera protectora para pintura.", costo: 20000, duracion: 30, excluye: ["pulida"] },
-  { nombre: "pulida", descripcion: "Pulido completo de la pintura, incluye encerado final.", costo: 80000, duracion: 120, excluye: ["encerado"] },
-  { nombre: "lavado-motor", descripcion: "Limpieza de compartimento del motor con productos especiales.", costo: 30000, duracion: 40 },
+  { nombre: "Encerado", categoria: "tratamientoEspecial", descripcion: "Aplicación de cera protectora para pintura.", costo: 20000, duracion: 30, excluye: ["Pulida"], url: 'https://i.postimg.cc/d1HFqzwx/encerado.jpg' },
+  { nombre: "Pulida", categoria: "tratamientoEspecial", descripcion: "Pulido completo de la pintura, incluye encerado final.", costo: 80000, duracion: 120, excluye: ["Encerado"], url: 'https://i.postimg.cc/5NwCt3Qr/pulida.png'},
+  { nombre: "Lavado Motor", categoria: "tratamientoEspecial", descripcion: "Limpieza de compartimento del motor con productos especiales.", costo: 30000, duracion: 40, url: 'https://i.postimg.cc/tR61r5kC/lavado-motor.jpg'},
 
   // ---- Extras ----
-  { nombre: "brillo-llantas", descripcion: "Aplicación de silicona para brillo y protección de llantas.", costo: 5000, duracion: 10 },
-  { nombre: "aromatizante", descripcion: "Perfume interior a elección del cliente.", costo: 3000, duracion: 5 },
-  { nombre: "lavado-debajo", descripcion: "Limpieza profunda del chasis y partes bajas del vehículo.", costo: 25000, duracion: 35 },
-  { nombre: "secado-detallado", descripcion: "Secado con microfibra y aire comprimido para eliminar gotas.", costo: 8000, duracion: 10 }
+  { nombre: "Brillo Llantas", categoria: "extra", descripcion: "Aplicación de silicona para brillo y protección de llantas.", costo: 5000, duracion: 10, url: 'https://i.postimg.cc/SRnhjDxH/brillo-llantas.jpg'},
+  { nombre: "Aromatizante", categoria: "extra", descripcion: "Perfume interior a elección del cliente.", costo: 3000, duracion: 5, url: 'https://i.postimg.cc/bJCPzKpK/aromatizante.jpg'},
+  { nombre: "Lavado Debajo", categoria: "extra", descripcion: "Limpieza profunda del chasis y partes bajas del vehículo.", costo: 25000, duracion: 35, url: 'https://i.postimg.cc/prkM041S/lavado-debajo.jpg' },
+  { nombre: "Secado Detallado", categoria: "extra", descripcion: "Secado con microfibra y aire comprimido para eliminar gotas.", costo: 8000, duracion: 10, url: 'https://i.postimg.cc/SRgHL8JV/secado-detallado.jpg'}
 ]);
 
 
