51b41f3e0277e1405818e856c8e0ef58a152c37e
Liang Camilo <liangcamilo2006@gmail.com>
2025-10-23 22:20:03 -0500

    minor changes

--- backend/schemas/servicioSchema.js
+++ backend/schemas/servicioSchema.js
@@ -2,7 +2,8 @@ import {Schema} from "mongoose";
 
 const servicioSchema = Schema({
     nombre: {type: String, required: true},
-    costo: {type: Number, required: true},
+    descripcion: {type: String, required: true},
+    costo: {type: String, required: true},
     duracion: {type: Number, required: true},
     excluye: {type: [String], default: []}
 })
