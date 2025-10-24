708937f054a62bbd38e20946a2ca72ecbd9d57b2
Liang Camilo <liangcamilo2006@gmail.com>
2025-10-23 22:21:03 -0500

    minor changes

--- backend/routes/reserva.routes.js
+++ backend/routes/reserva.routes.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import ReservationController from "../controllers/reserva.controller.js";
 import authVerification from "../middlewares/authVerification.js";
+import validate from '../middlewares/validate.js'
+import { filterScheme } from "../validations/reserveValidation.js";
 
 const reservationRouter = Router();
 const reservationController = new ReservationController();
 
 reservationRouter.post('/create', authVerification, reservationController.createReservation)
 reservationRouter.get('/my-reservations', authVerification, reservationController.getAllUserReservations)
-reservationRouter.post('/filter-reservations', reservationController.filterReservations)
+reservationRouter.post('/filter-reservations', validate(filterScheme), reservationController.filterReservations)
 
 export default reservationRouter
