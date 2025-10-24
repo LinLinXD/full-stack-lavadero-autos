c65ebf1d4d22f43897fb31b8c7c31cab2899763a
Liang Camilo <liangcamilo2006@gmail.com>
2025-10-24 15:20:31 -0500

    minor changes on services -- added url and categoria as properties

--- backend/schemas/servicioSchema.js
+++ backend/schemas/servicioSchema.js
@@ -2,10 +2,12 @@ import {Schema} from "mongoose";
 
 const servicioSchema = Schema({
     nombre: {type: String, required: true},
+    categoria: {type: String, required: true},
     descripcion: {type: String, required: true},
     costo: {type: String, required: true},
     duracion: {type: Number, required: true},
-    excluye: {type: [String], default: []}
+    excluye: {type: [String], default: []},
+    url: {type: String, required: true}
 })
 
 export default servicioSchema
