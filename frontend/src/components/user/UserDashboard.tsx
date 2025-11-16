639adda310a06def08f810266b4a0bfd4bf1f1df
Liang Camilo <liangcamilo2006@gmail.com>
2025-11-16 15:19:25 -0500

    small changes

--- frontend/src/components/user/UserDashboard.tsx
+++ frontend/src/components/user/UserDashboard.tsx
@@ -440,7 +440,7 @@ const calculateTotal = (servicios: Service[]) => {
                           <svg className="flex-shrink-0 mr-1.5 h-4 w-4 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
                             <path fillRule="evenodd" d="M4 4a2 2 0 012-2h8a2 2 0 012 2v12a1 1 0 110 2h-3a1 1 0 01-1-1v-2a1 1 0 00-1-1H9a1 1 0 00-1 1v2a1 1 0 01-1 1H4a1 1 0 110-2V4zm3 1h2v2H7V5zm2 4H7v2h2V9zm2-4h2v2h-2V5zm2 4h-2v2h2V9z" clipRule="evenodd" />
                           </svg>
-                          <span className="font-medium">Placa:</span> {reservation.placa}
+                          <span className="font-medium mr-1">{`Placa: `} </span> <span>{reservation.placa}</span>
                         </div>
                         <div className="flex items-center text-sm text-gray-600">
                           <svg className="flex-shrink-0 mr-1.5 h-4 w-4 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
